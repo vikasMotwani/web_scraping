60a76f1b159d4e9472ea7559c8f1ee61d8a756ad
vikasMotwani <motwanivikas987@gmail.com>
2024-08-04 23:30:10 -0500

    added a main function

--- draft.py
+++ draft.py
@@ -4,9 +4,9 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 import time
 from urllib.parse import urljoin
-import requests
+import re
 
-company = 'http://quantumcircuits.com/'
+company = 'https://www.harvey.ai/'
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
@@ -15,17 +15,8 @@ options.add_argument('--ignore-ssl-errors')
 driver = webdriver.Chrome(options=options)
 
 driver.get(company)
-time.sleep(10)
+time.sleep(30)
 response=driver.page_source.encode('utf-8').strip()
 soup = BeautifulSoup(response, 'html.parser')
-print(soup.find_all('a'))
-career_link = soup.find('a', href=True, text='Careers')
-print(career_link)
-if career_link:
-    career_href = urljoin(company, career_link['href'])
-print(career_href)
-try:
-        driver.get(career_href)
-        print(career_href)
-except Exception as e:
-        print(False)
+print(soup.prettify())
+career_link = soup.find_all('a', href=True)
